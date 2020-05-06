4e317357cb2157a97cc265f1c3c92913504464da
PepziC0les <Jullianpepito@gmail.com>
2020-05-05 18:21:13 -0700

    A

--- extract_content.py
+++ extract_content.py
@@ -1,27 +1,38 @@
 import re
 #from nltk.tokenize import ToktokTokenizer
+import shelve
+import csv
 from nltk.tokenize import RegexpTokenizer
 from nltk import sent_tokenize, pos_tag
-import requests
+import requests #Requests optional, content already in JSON files.
 from bs4 import BeautifulSoup
 from math import log10
 
 
 class ContentExtractor:
-    def __init__(self, url):
+    def __init__(self, url=None, path=None, jsonLine="", writeLoc=None):
+        self.jsonLine = jsonLine
+        self.writeLoc = writeLoc
         self.url = url
+        self.file = path
         self.wordFrequency = dict()
         self.tfIdMappings = dict()
         self.contentBlocks = []
         self.mostFrequentWord = ("", 0)
-        self.size = 0
+        self.numOfTerms = 0
         
 
+
     def extract_content(self):
-        page = requests.get(self.url)    
-        tokenized = RegexpTokenizer(r"\w+")
-        content = BeautifulSoup(page.content, features="lxml")
-    
+        #page = requests.get(self.url)    
+        tokenized = RegexpTokenizer(r"\w+")    
+        #self.jsonLine = ""
+        #with open(self.jsonFile, "r") as file:
+        #    data = file.read()
+        #    obj = json.loads(data)
+        #    jsonLine = obj["content"]
+        content = BeautifulSoup(self.jsonLine, features="lxml")
+        
         for i in content.find_all(['a', 'p', 'tb', 'body', re.compile('^h[1-6]$'), ]):
             line = i.get_text(strip=True)
             for sent in sent_tokenize(line):
@@ -29,30 +40,65 @@ class ContentExtractor:
                 if len(tokens) != 0:
                     self.contentBlocks.extend(tokens)
                     self.compute_frequency(tokens)
-                    
     
     
     
     def compute_frequency(self, tokens:list):
         for word in tokens:
-            self.size += 1
+            self.numOfTerms += 1
             word = word[0].lower()
             if len(word) >= 2:
-                if word in self.wordFrequency:
-                    self.wordFrequency[word] += 1
-                else:
-                    self.wordFrequency[word] = 1
+                with shelve.open("WORDDB", "w") as db:
+                    if word in self.wordFrequency:
+                        self.wordFrequency[word] += 1
                 
-                #print(self.wordFrequency[word], self.mostFrequentWord[1])
-                if self.wordFrequency[word] > self.mostFrequentWord[1]:
-                    self.mostFrequentWord = (word, self.wordFrequency[word])
+                    else:
+                        self.wordFrequency[word] = 1
+                
+                    if self.wordFrequency[word] > self.mostFrequentWord[1]:
+                        self.mostFrequentWord = (word, self.wordFrequency[word])
+    
+                    if word in db:
+                        db[word] += 1
+                    else:
+                        db[word] = 1
+
+        
+        
+
+    def write_to_file(self, url, tfidf, term):
+        with open('document_idrtf.csv', mode="w", newline='') as file:
+            document = csv.writer(file, dialect='excel', delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator = '\n')
+            document.writerow([url, term, tfidf])
     
     
-    def tf_idf_score(self, word):
-        tf = log10(1 + self.wordFrequency[word])
-        #idf = log10(self.size/)
+    def map_tfidf_term(self):
+        mappings = []
+        for i,j in self.tfIdMappings.items():
+            mappings.append(i, self.calculate_tfidf(t=i, ))
+    
+    #Cannot use until total number of documents have been counted for, as well as all words.
+    def calculate_tfidf(t, numOfDwithT, numOfTerms=self.numOfTerms, numOfD=57381):
+        numOfDinA = 1988
+        numOfDinD = 55393
+        tf = t/numOfDwithT
+        idf = math.log10(numOfD, numOfDwithT)
+        return tf * idf
 
 
-content = ContentExtractor("https://www.nltk.org/")
-content.extract_content()
-print(content.mostFrequentWord)
+if __name__ == "__main__":
+    content = ContentExtractor("https://www.nltk.org/")
+    content.extract_content()
+    print(content.mostFrequentWord)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- test.py
+++ test.py
@@ -1,37 +1,49 @@
 import os
 import json
-
-# 
+import time
+import csv 
+from extract_content import ContentExtractor
 
 def getJSONFiles(path: str):
     list_paths = os.listdir(path=path)
     # Goes until the list is empty
-    while len(list_paths) > 0:
-        # Checks to see if the next file is json
-        if(".json" in list_paths[-1]):
-            # Loads the json and prints the url
-            with open(list_paths[-1]) as file:
-                data = file.read()
-                obj = json.loads(data)
-                print(obj["url"])
+    with open('document_idrtf.csv', mode="w", newline='') as file2:
+        document = csv.writer(file2, dialect='excel', delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator = '\n')
+        while len(list_paths) > 0:
+            
+            # Checks to see if the next file is json
+            if(".json" in list_paths[-1]):
+                # Loads the json and prints the url
+                with open(list_paths[-1]) as file:
+                    data = file.read()
+                    obj = json.loads(data)
+                    #print()
+                    url = obj["url"]
+                    webHTML = obj["content"]
+                    content = ContentExtractor(jsonLine = webHTML)
+                    content.extract_content()
+                    
+                    document.writerow([url, term, tfidf])
+                    list_paths.pop()
+            else:
+                # Builds the new path using the next directory in the url
+                new_path = os.path.join(path, list_paths[-1])
+                #print(new_path)
+                # Loads the directories and builds the path to that directory
+                new_list = []
+                for val in os.listdir(path=new_path):
+                    new_list.append(os.path.join(new_path, val))
+                # deletes the last value then add to the start of the list
                 list_paths.pop()
-        else:
-            # Builds the new path using the next directory in the url
-            new_path = os.path.join(path, list_paths[-1])
-            # Loads the directories and builds the path to that directory
-            new_list = []
-            for val in os.listdir(path=new_path):
-                new_list.append(os.path.join(new_path, val))
-            # deletes the last value then add to the start of the list
-            list_paths.pop()
-            list_paths += new_list 
-
-    
-
-
+                list_paths += new_list 
 
 
 
 
 if __name__ == "__main__":
-    getJSONFiles(os.path.join(os.getcwd(), "DEV"))
+    #for i in ["ANALYST", "DEV"]:
+    for i in ["SAMPLE"]:
+        start = time.time()
+        getJSONFiles(os.path.join(os.getcwd(), i))
+        end = time.time()
+        print(end-start, i, "Brandon")
