7a49ac25f90d3dccc05442745a01c59f47578a25
NguyenBran <nguyen.brandon.0526@gmail.com>
2020-05-24 11:01:30 -0700

    trying to implement cosine sim

--- search_index.py
+++ search_index.py
@@ -1,9 +1,12 @@
 from sklearn.metrics.pairwise import cosine_similarity as csim
+import scipy.spatial.distance
 import ijson
 import json
 import os
 import time
 import itertools
+from numpy import dot
+from numpy.linalg import norm
 
 class Search():
     
@@ -26,7 +29,6 @@ class Search():
             with open(os.path.join(path, (i[0] + ".json")), "r", buffering=1) as file:
                 item = json.loads(file.read())
                 ptr.append((i, item[i]))
-        print("ptr: ", ptr)
         return ptr
     
     
@@ -54,27 +56,32 @@ class Search():
                         self._pages[j['url']].append(j['tfidf'])
                 #print(self._pages)
         self._pages = {url:tfidf for url, tfidf in self._pages.items() if len(tfidf) == len(self._components)}
-        vals = {url : (sum(tfidf)/len(tfidf)) for url, tfidf in self._pages.items()}
-        sorted_vals = sorted(vals, key=lambda x: vals[x], reverse=True)
+        qf = [self._components.count(word)/len(self._components) for word in self._components]
+        results = list()
+        print("qf: ", qf)
+        for url, val in self._pages.items():
+            print(val)
+            # qf is the list of the query frequency
+            # val is the tfidf of a url
+            # the scipy thing is something I saw online and by subtracting 1 from the distance u get the similarity apparently
+            # the second one is more like the formula with the dot product over the normalization
+            
+            #cosine_sim = 1 - scipy.spatial.distance.cosine(qf, val)
+            cosine_sim = dot(qf, val) / (norm(qf) * norm(val))
+            #if results[-1][1] < cosine_sim:
+            #    results.pop()
+            
+            #if len(results) > 10:
+            #    pass
+            #else:
+            #    results.append()
+            results.append((url, cosine_sim))
+        #vals = {url : (sum(tfidf)/len(tfidf)) for url, tfidf in self._pages.items()}
+        sorted_vals = sorted(results, key=lambda x: x[1], reverse=True)
+        #print(results)
         #print(sorted_vals)
-        for i in sorted_vals[:5]:
-            print("i: ", i, "tfidf: ", vals[i])
+        print("\nHere are the top 5 relevant websites!")
+        for count, i in enumerate(sorted_vals[:5]):
+            print(f"{count+1}: ", i)
+        print()
         return sorted_vals[:5]    
-        
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
