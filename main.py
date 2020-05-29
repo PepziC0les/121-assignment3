c9b05878b158322f5df8358a0e68fa22ff54a2a4
NguyenBran <nguyen.brandon.0526@gmail.com>
2020-05-29 13:28:20 -0700

    works for the most part, haven't run exhaustive tests yet

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from search_index import Search
+from test_search import Search
+from search_index import Search as S
 import time
-
+import json
 def main():
 
     while True:
         query = input("Search: ")
         if query == "quit()":
             break
+        start = time.time()
         results = Search(query)
-        print(results.get_pages())
+        pages = results.new_getPages()
+        end = time.time()
+        # this runs only when everything runs fine
+        if pages:
+            print("\nHere are the top 5 relevant websites!")
+            with open("docID_url.json", "r") as file:
+                data = json.load(file)
+            for count, i in enumerate(pages):
+                print(f"{count+1}: ", data[str(i[0])])
+            print()
+        
+        print(end - start)
+
         
 
 
--- test_search.py
+++ test_search.py
@@ -30,34 +30,50 @@ class Search():
 
     def new_getPages(self, bufferSize = 65534):
         path = os.path.join(os.getcwd(),"PTR", "JSON")
+        # gets pages for words that aren't stop words
         for word in self._components:
+            # picks the json file to look into
             if word[0] in "abcdefghijklmnopqrstuvwxyz":
                 temp_path = os.path.join(path, word[0] + ".json")
             else:
                 temp_path = os.path.join(path, "misc.json")
+            # Starts going through json file looking for word and respective ids
             with open(temp_path, "r") as file:
                 temp = file.read(bufferSize)
                 termValue = temp
                 start = time.time()
-                while time.time() - start < .02:
+                # gives the code .05 sec to look through the json
+                while time.time() - start < .05:
                     temp = file.read(bufferSize)
-                    
-                    if word in termValue:
+                    # if the word is in the termValue keep appending if it isn't replace what is in it
+                    if word in termValue.split("\""):
                         termValue += temp
                     else:
                         termValue = temp
-                    if termValue.find("]]") < termValue.find(word):
-                        termValue = termValue[termValue.index(word) - 1:]
+
+                    # find the spot of the word and ending ]]
+                    wordPos = termValue.find("\"" + word + "\"")
+                    endPos = termValue.find("]]")
+                    # remove extra characters behind if the ending is before the word
+                    if endPos < wordPos:
+                        termValue = termValue[wordPos:]
+                    # if the word and ending exists break
+                    if wordPos != -1 and wordPos < endPos:
+                        break
+                # formats the result into a json 
                 termValue = "{" + termValue[:termValue.find("]]")+2] + "}"
-                #with open("test.txt", "w") as file:
-                 #   file.write (termValue)
                 data = json.loads(termValue)
 
-                for val in data[word]:
-                    if val[0] in self._pages:
-                        self._pages[val[0]].append(val[1])
-                    else:
-                        self._pages[val[0]] = [val[1],]
+                # adds the url and its tfidf value to pages, throws exception if the word doesn't match what came out of the json
+                try:
+                    for val in data[word]:
+                        if val[0] in self._pages:
+                            self._pages[val[0]].append(val[1])
+                        else:
+                            self._pages[val[0]] = [val[1],]
+                except:
+                    print("Sorry, results for your search can not be found in our database")
+                    return
         if len(self._pages) < 10:
             for word in self._stopWords:
                 if word[0] in "abcdefghijklmnopqrstuvwxyz":
@@ -68,27 +84,36 @@ class Search():
                     temp = file.read(bufferSize)
                     termValue = temp
                     start = time.time()
-                    while time.time() - start < .02:
+                    while time.time() - start < .05:
                         temp = file.read(bufferSize)
-                        
-                        if word in termValue:
+                        if word in termValue.split("\""):
                             termValue += temp
                         else:
                             termValue = temp
-                        if termValue.find("]]") < termValue.find(word):
-                            termValue = termValue[termValue.index(word) - 1:]
-                    termValue = "{" + termValue[:termValue.find("]]")+2] + "}"
 
+                        wordPos = termValue.find("\"" + word + "\"")
+                        endPos = termValue.find("]]")
+                        if endPos < wordPos:
+                            termValue = termValue[wordPos:]
+                        if wordPos != -1 and wordPos < endPos:
+                            break
+                    termValue = "{" + termValue[:termValue.find("]]")+2] + "}"
                     data = json.loads(termValue)
 
-                    for val in data[word]:
-                        if val[0] in self._pages:
-                            self._backupPages[val[0]].append(val[1])
-                        else:
-                            self._backupPages[val[0]] = [val[1],]
+                    try:
+                        for val in data[word]:
+                            if val[0] in self._backupPages:
+                                self._backupPages[val[0]].append(val[1])
+                            else:
+                                self._backupPages[val[0]] = [val[1],]
+                    except:
+                        print("Sorry, results for your search can not be found in our database")
+                        return
+        # takes only pages with all existing words
         self._pages = {url:tfidf for url, tfidf in self._pages.items() if len(tfidf) == len(self._components)}
         self._backupPages = {url:tfidf for url, tfidf in self._backupPages.items() if len(tfidf) == len(self._stopWords)}
 
+        # query frequency
         qf = [self._components.count(word)/len(self._components) for word in self._components]
         results = list()
         done = False
@@ -104,12 +129,13 @@ class Search():
             if url in self._backupPages:
                 self._backupPages[url].append(val)
         
+        # if there aren't enough results, add to the number of results with the backup pages
         if not done:
             qf = [self._original.count(word)/len(self._original) for word in self._original]
 
             for url in sorted(self._backupPages, key= lambda x: sum(self._backupPages[x]), reverse=True)[:50]:
                 cosine_sim = dot(qf, self._backupPages[url]) / (norm(qf) * norm(self._backupPages[url]))
-                if cosine_sim > results[-1][1] or len(results) < 10:
+                if len(results) < 10 or cosine_sim > results[-1][1]:
                     if len(results) == 10:
                         results.pop()
                     results.append((url, cosine_sim))
